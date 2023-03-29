330fe7093aaeade9e16c94406af66e0bd6c1574b
fouadmo1223 <fm0850020@gmail.com>
2023-03-29 23:18:54 +0200

    fix otp requests and make the fields autofoucs

--- docs/en/js/confpass.js
+++ docs/en/js/confpass.js
@@ -1,5 +1,35 @@
     (async() => {
 
+        const inputs = document.querySelectorAll(".otp-field input");
+        inputs.forEach((input, index) => {
+            input.dataset.index = index;
+            input.addEventListener("keyup", handleOtp);
+            input.addEventListener("paste", handleOnPasteOtp);
+        });
+
+        function handleOtp(e) {
+            const input = e.target;
+            let value = input.value;
+            let isValidInput = value.match(/[0-9a-z]/gi);
+            input.value = "";
+            input.value = isValidInput ? value[0] : "";
+            let fieldIndex = input.dataset.index;
+            if (fieldIndex < inputs.length - 1 && isValidInput) {
+                input.nextElementSibling.focus();
+            }
+            if (e.key === "Backspace" && fieldIndex > 0) {
+                input.previousElementSibling.focus();
+            }
+        }
+
+        function handleOnPasteOtp(e) {
+            const data = e.clipboardData.getData("text");
+            const value = data.split("");
+            if (value.length === inputs.length) {
+                inputs.forEach((input, index) => (input.value = value[index]));
+            }
+        }
+
         $("#vpassword").keyup(function() {
             if ($("#vpassword").val() != $("#password").val()) {
                 document.querySelector(".confirm-pass").style.display = "block"
@@ -67,8 +97,8 @@
 
                     console.log(xhr)
                     if (errorMessages.hasOwnProperty('errors')) {
-                        if (errorMessages.hasOwnProperty('UsernameOrEmail')) {
-                            $(".confirm-pass").text(errorMessages.UsernameOrEmail);
+                        if (errorMessages.errors.hasOwnProperty('UsernameOrEmail')) {
+                            $(".confirm-pass").text(errorMessages.errors.UsernameOrEmail);
                             $(".confirm-pass").css("display", "block");
                             setTimeout(function() {
                                 window.location.href = "forgot.html"
--- docs/en/js/patotp.js
+++ docs/en/js/patotp.js
@@ -1,4 +1,36 @@
 (async() => {
+
+    const inputs = document.querySelectorAll(".otp-field input");
+    inputs.forEach((input, index) => {
+        input.dataset.index = index;
+        input.addEventListener("keyup", handleOtp);
+        input.addEventListener("paste", handleOnPasteOtp);
+    });
+
+    function handleOtp(e) {
+        const input = e.target;
+        let value = input.value;
+        let isValidInput = value.match(/[0-9a-z]/gi);
+        input.value = "";
+        input.value = isValidInput ? value[0] : "";
+        let fieldIndex = input.dataset.index;
+        if (fieldIndex < inputs.length - 1 && isValidInput) {
+            input.nextElementSibling.focus();
+        }
+        if (e.key === "Backspace" && fieldIndex > 0) {
+            input.previousElementSibling.focus();
+        }
+    }
+
+    function handleOnPasteOtp(e) {
+        const data = e.clipboardData.getData("text");
+        const value = data.split("");
+        if (value.length === inputs.length) {
+            inputs.forEach((input, index) => (input.value = value[index]));
+        }
+    }
+
+
     let apiUrl = await GetServerDomain();
     let language = window.localStorage.getItem("language")
     if (language == null) {
@@ -23,7 +55,13 @@
             console.log(data);
         },
         error: function(xhr, status, err) {
-            if (xhr.status == 500) {
+            if (xhr.status == 401) {
+                $(".wrong-pass").text("you are not authorithed");
+                $(".wrong-pass").css("display", "block");
+                setTimeout(function() {
+                    window.location.href = "../login.html"
+                }, 2000)
+            } else if (xhr.status == 500) {
                 $(".wrong-pass").text(xhr.responseJSON.error);
                 $(".wrong-pass").css("display", "block");
             } else {
